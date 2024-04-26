26a45ba8403db8be877a71794bd71b999aca8b12
Rizwanishere <rizwantopper@gmail.com>
2024-04-26 23:58:52 +0530

    ShouldRender Component

--- src/User/UserList.jsx
+++ src/User/UserList.jsx
@@ -2,6 +2,7 @@ import { Component } from "react";
 import axios from 'axios';
 import UserItem from './UserItem';
 import Error from '../util/Error';
+import ShouldRender from '../util/ShouldRender';
 
 class UserList extends Component{
 
@@ -13,7 +14,7 @@ class UserList extends Component{
     constructor(){
         super();
         
-        axios.get('https://api.github.com/users')
+        axios.get('https://api.github.com/users1') //Forced-Error
         .then(res => this.setState({ users: res.data }))
         .catch(() => this.setState({ hasError: true }));
     };
@@ -21,8 +22,10 @@ class UserList extends Component{
     render(){
         return(
             <div>
-                {this.state.hasError && <Error/>}
-                {/* {this.state.hasError && <Error msg="This is a customized error"/>} */}
+                {/* {this.state.hasError && <Error/>} */}
+                <ShouldRender when={this.state.hasError}>
+                    <Error msg="Customized Message"/>
+                </ShouldRender>
                 <h1 className="mt-2 ml-9 text-3xl font-semibold text-gray-500">Users</h1>
                 {this.state.users.map(user => <UserItem user={user}/>)}
             </div>
