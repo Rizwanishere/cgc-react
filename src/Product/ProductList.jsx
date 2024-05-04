78c5b05028cca8875e2254c1961a69d2a73b3fd8
Rizwanishere <rizwantopper@gmail.com>
2024-05-05 01:51:11 +0530

    Pagination

--- src/Product/ProductList.jsx
+++ src/Product/ProductList.jsx
@@ -10,22 +10,55 @@ class ProductList extends Component {
     products: [],
     hasError: false,
     loading: true,
+    page: 1,
+    metadata: {}
+  };
+
+  onPrev = () => {
+    if (this.state.page > 1) {
+      this.setState({
+        page: this.state.page - 1,
+      });
+    }
+    console.log("onPrev");
+  };
+
+  onNext = () => {
+    if (this.state.page < this.state.metadata.pages) {
+      this.setState({
+        page: this.state.page + 1,
+      });
+      console.log("onNext");
+    }
   };
 
   constructor() {
     super();
-    axios
-      .get("http://localhost:3000/products")
-      .then((res) => this.setState({ products: res.data.data }))
+  }
+
+  fetchData() {
+    const url = `http://localhost:3000/products/page/${this.state.page}/size/9`;
+    axios.get(url)
+      .then((res) => this.setState({ products: res.data.data, metadata: res.data.metadata}))
       .catch((err) => this.setState({ hasError: true }))
       .finally(() => this.setState({ loading: false }));
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  componentDidUpdate(a, b) {
+    if (b.page != this.state.page) {
+      this.fetchData();
+    }
   }
 
   render() {
     return (
       <div>
         <div>
-          <h1 className="text-2xl font-semibold text-gray-500 m-2 flex">
+          <h1 className="text-2xl font-semibold text-gray-500 m-2 flex p-1">
             Products
             <ShouldRender when={this.state.loading}>
               <Loader />
@@ -33,6 +66,51 @@ class ProductList extends Component {
             <ShouldRender when={this.state.hasError}>
               <Error />
             </ShouldRender>
+            <button
+              onClick={this.onPrev}
+              style={{ backgroundColor: this.state.page === 1 ? "gray" : '' }}
+              className="bg-orange-500 m-1 ml-3 text-white hover:bg-orange-600 p-1 rounded"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15.75 19.5 8.25 12l7.5-7.5"
+                />
+              </svg>
+            </button>
+            <h1 className="m-1 text-gray-500 font-normal text-lg">
+              {this.state.page} of {this.state.metadata.pages}
+              {' '}
+              (Total: {this.state.metadata.rows})
+            </h1>
+            <button
+              onClick={this.onNext}
+              style={{ backgroundColor: this.state.page === this.state.metadata.pages ? "gray" : '' }}
+              className="bg-orange-500 m-1 text-white hover:bg-orange-600 p-1 rounded"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                />
+              </svg>
+            </button>
           </h1>
         </div>
 
