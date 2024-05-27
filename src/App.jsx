f873510cf312f4bcb4a35471cadeaf31c59420ef
Rizwanishere <rizwantopper@gmail.com>
2024-05-27 14:57:13 +0530

    Fixed Logout button disapperance

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./Header";
 import MainApp from "./MainApp";
@@ -6,7 +6,18 @@ import Footer from "./Footer";
 import UserContext from "./context/UserContext";
 
 const App = () => {
-  const [isLoggedin, setLoggedin] = useState(false);
+  const [isLoggedin, setLoggedin] = useState(() => {
+    return localStorage.getItem('token') ? true : false;
+  });
+// Fixed Issue: When reloading the page, Logout button was getting disappeared
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setLoggedin(true);
+    } else {
+      setLoggedin(false);
+    }
+  }, []);
 
   return (
     <div className="flex flex-col min-h-screen justify-between">
