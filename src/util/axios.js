b56dcba5955459f403526ed2e3aa7e04ee569461
Rizwanishere <rizwantopper@gmail.com>
2024-05-27 14:53:50 +0530

    Added Deployed api link

--- src/util/axios.js
+++ src/util/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const getInstance = () => {
-  const host = "http://localhost:3000";
+  const host = "https://cgc-nodejs.onrender.com";
   const headers = {};
   if (localStorage.getItem("token")) {
     const token = localStorage.getItem("token");
